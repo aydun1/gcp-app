4f1d2316f33e3cc1146d44d9ab898142d6693d75
Aydun <ayduno@gmail.com>
2021-11-05 14:43:16 +1000

    Update pallets automatically

--- src/app/customers/customer-view/customer-view.component.ts
+++ src/app/customers/customer-view/customer-view.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { filter, map, switchMap, tap } from 'rxjs/operators';
 import { CustomersService } from '../shared/customers.service';
 import { PalletDialogComponent } from '../shared/pallet-dialog/pallet-dialog.component';
@@ -16,7 +16,9 @@ import { RecyclingDialogComponent } from '../shared/recycling-dialog/recycling-d
 export class CustomerViewComponent implements OnInit {
   private id: string;
   private navigationSubscription: Subscription;
+  private palletsSubject$ = new Subject<string>();
   public customer$: Observable<any>;
+  public pallets: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -29,10 +31,20 @@ export class CustomerViewComponent implements OnInit {
   ngOnInit(): void {
     this.customer$ = this.getCustomer();
 
+    this.palletsSubject$.pipe(
+      switchMap(id => this.cutomersService.getPallets(id)),
+      map(pallets => {
+        const loscams = pallets.filter(_ => _.fields.Pallet === 'Loscam').reduce((acc, curr) => acc + parseInt(curr.fields.Change), 0);
+        const cheps = pallets.filter(_ => _.fields.Pallet === 'Chep').reduce((acc, curr) => acc + parseInt(curr.fields.Change), 0);
+        const plains =pallets.filter(_ => _.fields.Pallet === 'Plain').reduce((acc, curr) => acc + parseInt(curr.fields.Change), 0);
+        return {pallets, loscams, cheps, plains};
+      })
+    ).subscribe(pallets => this.pallets = pallets);
+
     this.navigationSubscription = this.router.events.pipe(
       filter((e): e is NavigationEnd => e instanceof NavigationEnd),
       tap(_ => {if (this.id !==  this.route.snapshot.paramMap.get('id')) this.customer$ = this.getCustomer()})
-    ).subscribe()
+    ).subscribe();
 
   }
 
@@ -43,46 +55,30 @@ export class CustomerViewComponent implements OnInit {
   getCustomer() {
     this.id = this.route.snapshot.paramMap.get('id');
     return this.cutomersService.getCustomer(this.id).pipe(
-      switchMap(customer => this.cutomersService.getPallets(customer.accountnumber).pipe(
-        map(pallets => {
-          customer['pallets'] = pallets;
-          console.log(customer['loscam'])
-          customer['loscams'] = pallets.filter(_ => _.fields.Pallet === 'Loscam').reduce((acc, curr) => acc + parseInt(curr.fields.Change), 0);
-          customer['cheps'] = pallets.filter(_ => _.fields.Pallet === 'Chep').reduce((acc, curr) => acc + parseInt(curr.fields.Change), 0);
-          customer['plains'] = pallets.filter(_ => _.fields.Pallet === 'Plain').reduce((acc, curr) => acc + parseInt(curr.fields.Change), 0);
-          console.log(customer['loscams'])
-
-          return customer;
-        })
-      )),
-      tap(_ => console.log(_))
+      tap(_ => this.refreshPallets(_.accountnumber))
     );
+  }
+
+  refreshPallets(id: string) {
+    this.palletsSubject$.next(id);
   }
 
   openPalletDialog(customer: string) {
     const data = {customer};
-
-    const dialogRef = this.dialog.open(PalletDialogComponent, {
-      width: '600px',
-      data
-    });
+    const dialogRef = this.dialog.open(PalletDialogComponent, {width: '600px', data});
     dialogRef.afterClosed().subscribe(result => {
+      this.refreshPallets(data.customer);
       if (!result) return;
     });
   }
 
   openRecyclingDialog(customer: string) {
     const data = {customer};
-
-    const dialogRef = this.dialog.open(RecyclingDialogComponent, {
-      width: '600px',
-      data
-    });
+    const dialogRef = this.dialog.open(RecyclingDialogComponent, {width: '600px', data});
     dialogRef.afterClosed().subscribe(result => {
       if (!result) return;
     });
   }
-
 
   goBack() {
     this.location.back();
