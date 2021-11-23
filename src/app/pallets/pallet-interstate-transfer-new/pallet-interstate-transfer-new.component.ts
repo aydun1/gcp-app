bd30a8131f67f560eb4830038acc606500bd71d7
Aydun <ayduno@gmail.com>
2021-11-23 15:37:03 +1000

    From to to

--- src/app/pallets/pallet-interstate-transfer-new/pallet-interstate-transfer-new.component.ts
+++ src/app/pallets/pallet-interstate-transfer-new/pallet-interstate-transfer-new.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { catchError, tap, throwError } from 'rxjs';
 
@@ -21,13 +20,12 @@ export class PalletInterstateTransferNewComponent implements OnInit {
   public state: string;
   public loading: boolean;
   get targetStates() {
-    return this.states.filter(_ => _ !== this.palletTransferForm.get('from').value);
+    return this.states.filter(_ => _ !== this.palletTransferForm.get('to').value);
   }
 
   constructor(
     private fb: FormBuilder,
     private location: Location,
-    private router: Router,
     private snackBar: MatSnackBar,
     private sharedService: SharedService,
     private palletService: PalletsService
@@ -39,14 +37,14 @@ export class PalletInterstateTransferNewComponent implements OnInit {
 
     this.sharedService.getState().subscribe(state => {
       this.state = state;
-      if (this.palletTransferForm) this.palletTransferForm.patchValue({from: state});
+      if (this.palletTransferForm) this.palletTransferForm.patchValue({to: state});
     });
 
     this.palletTransferForm = this.fb.group({
       date: [{value: date, disabled: true}, Validators.required],
       name: [{value: name, disabled: true}, Validators.required],
-      from: [{value: this.state, disabled: true}, Validators.required],
-      to: ['', [Validators.required]],
+      from: ['', Validators.required],
+      to: [{value: this.state, disabled: true}, [Validators.required]],
       type: ['', [Validators.required]],
       quantity: ['', [Validators.required, Validators.min(0)]],
       reference: ['', [Validators.required]]
@@ -54,18 +52,18 @@ export class PalletInterstateTransferNewComponent implements OnInit {
   }
 
   reset(): void {
-    const from = this.palletTransferForm.get('from').value;
+    const to = this.palletTransferForm.get('to').value;
     const name = this.sharedService.getName();
     const date = new Date();
-    this.palletTransferForm.reset({date, name, from});
+    this.palletTransferForm.reset({date, name, to});
     this.palletTransferForm.updateValueAndValidity()
   }
 
   onSubmit(): void {
     if (this.palletTransferForm.invalid) return;
     this.loading = true;
-    const from = this.palletTransferForm.get('from').value;
-    const payload = {...this.palletTransferForm.value, from};
+    const to = this.palletTransferForm.get('to').value;
+    const payload = {...this.palletTransferForm.value, to};
     this.palletService.interstatePalletTransfer(payload).pipe(
       tap(_ => {
         this.location.back();
