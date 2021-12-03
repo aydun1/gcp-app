57ba78ba15afa267a462354abf281d399ec5e12c
Aydun <ayduno@gmail.com>
2021-12-03 14:02:09 +1000

    Working

--- src/app/shared/upload.service.ts
+++ src/app/shared/upload.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http';
-import { lastValueFrom, map, Observable, switchMap, tap } from 'rxjs';
+import { from, lastValueFrom, map, mergeMap, Observable, of, switchMap, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UploadService {
   private endpoint = 'https://graph.microsoft.com/v1.0/sites/c63a4e9a-0d76-4cc0-a321-b2ce5eb6ddd4';
   private dataGroupUrl = 'sites/c63a4e9a-0d76-4cc0-a321-b2ce5eb6ddd4/drive/items/38f14082-02e5-4978-bf92-f42be2220166';
-  private palletTrackerUrl = `${this.endpoint}/${this.dataGroupUrl}`;
   private chunkLength = 320 * 1024;
 
   constructor(
@@ -24,8 +23,7 @@ export class UploadService {
       }
     }
     return this.http.post(url, payload).pipe(
-      tap(_ => console.log(_)),
-      switchMap((res: {uploadUrl: string}) => this.uploadChunks(file, res.uploadUrl)),
+      switchMap((res: {uploadUrl: string}) => this.uploadChunks(file, res.uploadUrl).pipe(tap(_ => console.log(_)))),
       tap(_ => console.log(_))
     );
   }
@@ -34,21 +32,22 @@ export class UploadService {
     const position = 0;
     const start = 0;
     let stop = position + this.chunkLength;
-    this.readFragment(file, start, stop, uploadUrl)
+    return this.readFragment(file, start, uploadUrl)
   }
 
 
-  readFragment(file: File, start: number, stop: number, url: string) {
-    const blob = file.slice(start, stop);
-    return blob.arrayBuffer().then(arrayBuffer => this.uploadChunk(file, arrayBuffer, url, start, file.size));
+  readFragment(file: File, start: number, url: string) {
+    return from(file.slice(start, start + this.chunkLength).arrayBuffer()).pipe(
+      switchMap(chunk => this.uploadChunk(file, chunk, url, start, file.size))
+    )
   }
 
-  uploadChunk(file: File, chunk: any | ArrayBuffer, uploadURL: string, position: number, totalLength: number) {   
-    const max = position + chunk.byteLength - 1;
-    const crHeader = `bytes ${position}-${max}/${totalLength}`;
-    return lastValueFrom(this.http.put(uploadURL, chunk, {headers: {'Content-Range': crHeader}})).then((res: {uploadUrl: string}) => 
-      this.readFragment(file, max, max+this.chunkLength, uploadURL)
-    );
+  uploadChunk(file: File, chunk: ArrayBuffer, uploadURL: string, start: number, totalLength: number) {   
+    const max = start + chunk.byteLength - 1;
+    const crHeader = `bytes ${start}-${max}/${totalLength}`;
+    return this.http.put(uploadURL, chunk, {headers: {'Content-Range': crHeader}}).pipe(
+      switchMap(_ => _['id'] ? of(_) : this.readFragment(file, max, uploadURL))
+    )
   }
 
 
