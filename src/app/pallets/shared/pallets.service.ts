901fcc219fc04c5488a18694b2f4b5aeda13bde3
Aydun <ayduno@gmail.com>
2021-12-03 11:52:22 +1000

    eod

--- src/app/pallets/shared/pallets.service.ts
+++ src/app/pallets/shared/pallets.service.ts
@@ -80,8 +80,9 @@ export class PalletsService {
   }
 
   getOwed(branch: string, pallet: string, date: Date) {
+    const eod = new Date(date.setHours(23,59,59,999)).toISOString();
     let url = `${this.palletTrackerUrl}/items?expand=fields(select=In,Out)&filter=fields/Branch eq '${branch}' and fields/Pallet eq '${pallet}' and fields/CustomerNumber ne null`;
-    url += ` and fields/Created lt '${date.toISOString()}'`;
+    url += ` and fields/Created lt '${eod}'`;
     return this.http.get(url).pipe(
       //tap(_ => this._nextPage = paginate ? _['@odata.nextLink'] : this._nextPage),
       map((res: {value: Pallet[]}) => res.value.reduce((acc, cur) => acc + +cur.fields.Out - +cur.fields.In, 0))
