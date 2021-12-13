42b52ed95f8c4a5198c5e79c678726ce48d89e22
Aydun <ayduno@gmail.com>
2021-12-14 09:42:55 +1000

    Start to rec view componet

---
+++ src/app/pallets/pallet-reconciliation-view/pallet-reconciliation-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gcp-pallet-reconciliation-view',
+  templateUrl: './pallet-reconciliation-view.component.html',
+  styleUrls: ['./pallet-reconciliation-view.component.css']
+})
+export class PalletReconciliationViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
