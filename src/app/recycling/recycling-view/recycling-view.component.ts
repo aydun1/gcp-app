a21e8a736513d6001b84e0bc46219d5da46cf65a
Aydun <ayduno@gmail.com>
2022-01-06 11:18:50 +1000

    Add a reference number field to cages

--- src/app/recycling/recycling-view/recycling-view.component.ts
+++ src/app/recycling/recycling-view/recycling-view.component.ts
@@ -20,6 +20,9 @@ export class RecyclingViewComponent implements OnInit {
   public displayedColumns = ['updated', 'customer', 'weight'];
   public totalWeight: number;
   public isCage: boolean;
+  public editCageNotes: boolean;
+  public currentCageNotes: string;
+  public newCageNotes: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -31,7 +34,8 @@ export class RecyclingViewComponent implements OnInit {
     this.cage$ = combineLatest([this.route.paramMap, this.cageSource$]).pipe(
       switchMap(_ => this.recyclingService.getCage(_[0].get('id'))),
       tap(_ => this.isCage = _.fields.AssetType.startsWith('Cage')),
-      tap(_ => this.getCageHistory(_.fields.CageNumber))
+      tap(_ => this.currentCageNotes = _.fields.Notes),
+      tap(_ => this.getCageHistory(_.fields.CageNumber)),
     );
     this.getCage();
   }
@@ -45,6 +49,20 @@ export class RecyclingViewComponent implements OnInit {
       tap(cages => this.totalWeight = cages.map(_ => _.fields.NetWeight).filter(_ => _).reduce((acc, val) => acc + val, 0)),
       tap(_ => this.noHistory = _.length === 0)
     );
+  }
+
+  setCageNotes(id: string) {
+    this.recyclingService.setNotes(id, this.currentCageNotes).pipe(
+      tap(() => {
+        this.cageSource$.next();
+        this.editCageNotes = false;
+      })
+    ).subscribe()
+  }
+
+  cancelEditCageNotes(notes: string) {
+    this.currentCageNotes = notes;
+    this.editCageNotes = false;
   }
 
   goBack() {
