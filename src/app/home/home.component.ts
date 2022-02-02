65b036180500c5a0a10397f10992c96574209b40
aydun1 <ayduno@gmail.com>
2022-02-02 14:19:37 +1000

    Remove some redundant things

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,34 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
-import { EventMessage, EventType, InteractionStatus } from '@azure/msal-browser';
-import { filter } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'gcp-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  public loginDisplay: boolean;
+export class HomeComponent {
 
   constructor(
-    private authService: MsalService,
-    private msalBroadcastService: MsalBroadcastService
   ) { }
 
-  ngOnInit(): void {
-    this.setLoginDisplay();
-
-    this.msalBroadcastService.msalSubject$.pipe(
-      filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS),
-    ).subscribe((result: EventMessage) => console.log(result));
-
-    this.msalBroadcastService.inProgress$.pipe(
-      filter((status: InteractionStatus) => status === InteractionStatus.None)
-    ).subscribe(() => this.setLoginDisplay());
-  }
-
-  setLoginDisplay(): void {
-    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
-  }
 }
