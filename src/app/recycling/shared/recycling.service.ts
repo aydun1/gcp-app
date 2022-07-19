c68bae4846846af57678739275994d730a8a4c3a
aydun1 <ayduno@gmail.com>
2022-07-19 12:11:00 +1000

    Fix for what is an active cage

--- src/app/recycling/shared/recycling.service.ts
+++ src/app/recycling/shared/recycling.service.ts
@@ -199,7 +199,8 @@ export class RecyclingService {
   getActiveCagesWithCustomer(custnmbr: string, site = ''): Observable<Cage[]> {
     const fields = ['AssetType', 'CageNumber', 'CageWeight', 'Created', 'CustomerNumber', 'Date1', 'Date2', 'Date3', 'Date4', 'GrossWeight', 'Modified', 'NetWeight', 'Site', 'Status', 'ToLocalProcessing'];
     let url = this._cageTrackerUrl + `/items?expand=fields(select=${fields.join(',')})&filter=fields/CustomerNumber eq '${this.shared.sanitiseName(custnmbr)}'`;
-    url += `and fields/Date2 eq null'`;
+    url += ` and fields/Date2 eq null`;
+    url += ` and fields/Status ne 'Complete'`;
     if (site) url += `and fields/Site eq '${this.shared.sanitiseName(site)}'`;
     return this.getCages(url);
   }
