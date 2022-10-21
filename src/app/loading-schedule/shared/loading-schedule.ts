e58fb69f0d38eb012e61e7c0d5d348646ae6d511
aydun1 <ayduno@gmail.com>
2022-10-21 15:11:30 +1000

    Nice printing on pan list

--- src/app/loading-schedule/shared/loading-schedule.ts
+++ src/app/loading-schedule/shared/loading-schedule.ts
@@ -1,14 +1,16 @@
 export interface LoadingSchedule {
-  createdDateTime: string | Date,
-  lastModifiedDateTime: string | Date,
-  id: string,
+  createdDateTime: string | Date;
+  lastModifiedDateTime: string | Date;
+  id: string;
   fields: {
-    Reference: string,
-    Branch: string,
-    NetWeight: number,
-    Spaces: number,
-    Status: string,
-    PanLists: string,
-    PanListsArray?: Array<string[]>,
+    Reference: string;
+    Branch: string;
+    Driver: string;
+    NetWeight: number;
+    Spaces: number;
+    Status: string;
+    PanLists: string;
+    PanListsArray?: Array<string[]>;
+    TransportCompany: string;
   }
 }
