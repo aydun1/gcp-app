5b90483a07e330c9685cbfa6fc0411f35f928c8f
aydun1 <ayduno@gmail.com>
2022-12-15 15:07:57 +1000

    Fix link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   redirectUri: 'https://pallets.gardencityplastics.com',
   gpEndpoint: `${apiUrl}/gp`,
   cwEndpoint: `${apiUrl}/cw`,
+  sdsEndpoint: `${apiUrl}/public`,
   endpoint: 'https://graph.microsoft.com/v1.0',
   siteUrl: 'sites/c63a4e9a-0d76-4cc0-a321-b2ce5eb6ddd4',
 };
