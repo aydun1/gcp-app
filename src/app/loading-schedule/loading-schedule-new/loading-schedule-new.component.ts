4ad479d252f0f1defb0eecc69e2d611f93605c16
aydun1 <ayduno@gmail.com>
2023-05-29 13:13:06 +1000

    International pan list

--- src/app/loading-schedule/loading-schedule-new/loading-schedule-new.component.ts
+++ src/app/loading-schedule/loading-schedule-new/loading-schedule-new.component.ts
@@ -35,7 +35,7 @@ export class LoadingScheduleNewComponent implements OnInit {
   public savingData = false;
   public transportCompanies$!: Observable<TransportCompany[] | null>;
   public loadingScheduleForm!: FormGroup<LoadingScheduleForm>;
-  public states = this.sharedService.branches;
+  public states = [...this.sharedService.branches, 'International'];
   public state!: string;
   public choices!: {TransportCompany: choice, Driver: choice, AssetType: choice, Branch: choice, Status: choice};
   public id: string | null = null;
